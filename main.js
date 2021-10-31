b22783ca3bca131b6d88437a112fa7b2be24cb0e
Vinicius Espindola <63016695+vespindola@users.noreply.github.com>
2021-10-31 10:59:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMidia = {
+    github: "jakeliny",
+    youtube: "jakelinygracielly",
+    facebook: "maikybrito",
+    instagram: "jakeliny.gracielly",
+    twitter: "jakelinytec"
+}
+
+function changeSocialMidliaLinks (){
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `http://${social}.com/${linksSocialMidia[social]}`
+
+    }   
+}
+
+changeSocialMidliaLinks()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMidia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name;
+        userBio.textContent  = data.bio;
+        userLink.href = data.html_url;
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
